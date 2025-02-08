e7885e528a794f62013f802b33e231265452af5c
Shreejagtap <94234886+Shreejagtap@users.noreply.github.com>
2025-02-08 19:53:05 +0530

    Added Sanity project id to env

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: "3xth7hpm",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-03-07",
   useCdn: true,
